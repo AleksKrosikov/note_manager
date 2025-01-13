d0a8380926384c19f4bac711b67c227c91a78f47
AleksKrosikov <krosikov1992@gmail.com>
2025-01-13 19:11:00 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,8 @@
+username = input("Введите имя пользователя: ")
+title = input("Заголовок: ")
+content = input("Описание: ")
+status = input("Статус заметки: ")
+created_date = input("Дата создания заметки, день-месяц-год: ")
+issue_date = input("Дата истечения заметки, день-месяц-год: ")
+
+print("Имя пользователя:", username)
